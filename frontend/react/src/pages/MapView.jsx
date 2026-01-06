4b5a20c9b2ad1e4b96aa227911a8618b2b962a90
Kaitlin Chen <kaitlinchen@Kaitlins-MacBook-Air.local>
2026-01-06 14:29:39 -0800

    set up files for the app

---
+++ frontend/react/src/pages/MapView.jsx
@@ -0,0 +1,12 @@
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+export default function MapView() {
+  return (
+    <p>Hello World</p>
+    // <MapContainer center={[20, 0]} zoom={2} style={{ height: "100vh" }}>
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //   />
+    // </MapContainer>
+  );
+}
